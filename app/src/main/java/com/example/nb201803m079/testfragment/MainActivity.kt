ef5149c32645af099c2d3844dc1fc6c679a7d498
田所みづほ <mizuho.tadokoro@nijibox.co.jp>
2019-01-05 23:17:23 +0900

    first commit

---
+++ app/src/main/java/com/example/nb201803m079/testfragment/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.nb201803m079.testfragment
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
